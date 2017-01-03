bacad07fa05afeaaa8b058464181ffdf8aea0f03
DanielJunior <danieljunior@id.uff.br>
2017-01-03 17:50:53 -0200

    Home Screen

---
+++ README.md
@@ -0,0 +1,1 @@
+De Olho na Escola
--- app/src/main/java/br/com/danieljunior/deolhonaescola/MainActivity.java
+++ app/src/main/java/br/com/danieljunior/deolhonaescola/MainActivity.java
@@ -1,13 +1,46 @@
 package br.com.danieljunior.deolhonaescola;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+import br.com.danieljunior.deolhonaescola.adapters.GridViewAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    GridView gv;
+    Context context;
+    ArrayList actionsName;
+    public static String [] actionsNameList={"Busca textual","Busca no Mapa", "Estatísticas Gerais"};
+    public static int [] actionsImages ={R.drawable.text_search,R.drawable.map_search,R.drawable.chart,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gv=(GridView) findViewById(R.id.gridView);
+        gv.setAdapter(new GridViewAdapter(this, actionsNameList, actionsImages));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.info:
+//                HERE GOES THE CODE TO INFO DIALOG
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
